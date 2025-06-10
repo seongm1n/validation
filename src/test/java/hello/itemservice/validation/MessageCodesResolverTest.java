af7094c0e0f1362d304d90014f564550c5841d26
Theo <0411tjdals34@gmail.com>
2025-06-10 12:06:06 +0900

    feat: 오류 코드와 메시지 처리

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,6 +1,5 @@
 package hello.itemservice.validation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
